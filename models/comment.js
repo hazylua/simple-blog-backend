e9722b61c9020b978aadb2bbe64c8784080d6cb7
yb00 <40061395+yb00@users.noreply.github.com>
2020-12-17 22:55:47 -0300

    Update comment model

--- models/comment.js
+++ models/comment.js
@@ -1,7 +1,7 @@
-const Joi = require("joi")
-const mongoose = require("mongoose")
+const Joi = require("joi");
+const mongoose = require("mongoose");
 
-const collection_name = "comments"
+const collection_name = "comments";
 
 const commentSchema = new mongoose.Schema(
   {
@@ -19,37 +19,37 @@ const commentSchema = new mongoose.Schema(
     },
   },
   { collection: collection_name }
-)
+);
 
-const Comment = mongoose.model("Comment", commentSchema)
+const Comment = mongoose.model("Comment", commentSchema);
 
 const joi_author_msgs = {
   "any.required": `A name needs to be provided.`,
   "string.base": `The name should be of type "text".`,
   "string.empty": `The name should not be empty.`,
-}
+};
 
 const joi_date_msgs = {
   "any.required": `A date needs to be provided.`,
   "date.base": `The date should be a valid "date" type.`,
   "date.empty": `The date should not be empty.`,
-}
+};
 
 const joi_body_msgs = {
   "any.required": `A comment needs to be provided.`,
-  "array.base": `The body of this comment is not valid".`,
-  "array.empty": `The comment should not be empty.`,
-}
+  "strin.base": `The body of this comment is not valid".`,
+  "string.empty": `The comment should not be empty.`,
+};
 
-const validateComment = body => {
+const validateComment = (body) => {
   const schema = Joi.object({
     author: Joi.string().required().messages(joi_author_msgs),
     date: Joi.date().required().messages(joi_date_msgs),
     body: Joi.string().required().messages(joi_body_msgs),
-  })
-  return schema.validate(body)
-}
+  }).unknown();
+  return schema.validate(body);
+};
 
-exports.commentSchema = commentSchema
-exports.Comment = Comment
-exports.validateComment = validateComment
+exports.commentSchema = commentSchema;
+exports.Comment = Comment;
+exports.validateComment = validateComment;
