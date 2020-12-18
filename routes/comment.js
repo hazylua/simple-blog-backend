1de1d99f1387ec350ca68131ed178a5596b4823d
yb00 <40061395+yb00@users.noreply.github.com>
2020-12-17 22:55:37 -0300

    Update comment get route

--- routes/comment.js
+++ routes/comment.js
@@ -1,18 +1,18 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const { validateComment } = require("../models/comment")
-const validateMiddleware = require("../middleware/joi-validator")
-const postController = require("../controllers/post-controller")
-const authenticationMiddleware = require("../middleware/authentication")
+const { validateComment } = require("../models/comment");
+const validateMiddleware = require("../middleware/joi-validator");
+const postController = require("../controllers/post-controller");
+const authenticationMiddleware = require("../middleware/authentication");
 
 router.post(
   "/",
   authenticationMiddleware(),
   validateMiddleware(validateComment),
   postController.commentAdd
-)
+);
 
-router.get("/", postController.postGetAll)
+router.get("/", postController.commentsGetAll);
 
-module.exports = router
+module.exports = router;
